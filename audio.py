bed304ac8f44672d0435e1ed2cdc5ffc3595f2b6
indatech <guainaisait@gmail.com>
2026-09-04 19:17:50 -0400

    implementacion del modulo de audio

--- audio.py
+++ audio.py
@@ -1,10 +1,26 @@
 # audio.py
+from pathlib import Path
+
+import pygame as py
+
+
 class SoundPlayer:
     def __init__(self):
-        pass
-        
+        py.mixer.init()
+        self.menu_music = Path("assets(beta)/audio/music.mp3")
+        self.game_music = Path("assets(beta)/audio/game_music.mp3")
+
+    def _play_track(self, track_path):
+        py.mixer.music.stop()
+
+        if not track_path.exists():
+            return
+
+        py.mixer.music.load(track_path.as_posix())
+        py.mixer.music.play(-1)
+
     def play_menu_music(self):
-        print("🎵 [Audio Falso] Sonando música del menú...")
-        
-    def play_game_music(self, bg_type):
-        print("🎵 [Audio Falso] Sonando música del juego...")
+        self._play_track(self.menu_music)
+
+    def play_game_music(self, bg_type=0):
+        self._play_track(self.game_music)
